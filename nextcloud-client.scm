75361766c80ca9b7acb589e4a3abddaef8ad67c1
Eric Brown <brown@fastmail.com>
2019-08-24 19:18:51 -0500

    Add license and other cleanup

--- nextcloud-client.scm
+++ nextcloud-client.scm
@@ -1,26 +1,32 @@
+;;; Copyright © 2019 Eric Brown <brown@fastmail.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (nextcloud-client)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build-system cmake)
-;  #:use-module (guix build-system go)
-;  #:use-module (guix build-system meson)
-;  #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module (gnu packages)
-;  #:use-module (gnu packages acl)
   #:use-module (gnu packages check)
   #:use-module (gnu packages compression)
-;  #:use-module (gnu packages curl)
-;  #:use-module (gnu packages golang)
   #:use-module (gnu packages glib)
-;  #:use-module (gnu packages linux)
-;  #:use-module (gnu packages lua)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages qt)
-;  #:use-module (gnu packages rsync)
-;  #:use-module (gnu packages selinux)
-;  #:use-module (gnu packages sphinx)
   #:use-module (gnu packages sqlite)
   #:use-module (gnu packages tls))
 
