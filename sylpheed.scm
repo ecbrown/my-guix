6598e80b56f16ef2c27ecb00dc869ca5dccd0e6e
Eric Brown <brown@fastmail.com>
2019-08-25 22:39:16 -0500

    sylpheed.scm: Fix gtkspell3

--- sylpheed.scm
+++ sylpheed.scm
@@ -1,3 +1,20 @@
+;;; Copyright © 2019 Eric Brown <brown@fastmail.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (sylpheed)
   #:use-module (gnu packages)
   #:use-module (gnu packages autotools)
@@ -36,7 +53,7 @@
        ("gnupg" ,gnupg-1)
        ("gpgme" ,gpgme)
        ("gtk+-2.0" ,gtk+-2)
-       ("gtkspell" ,gtkspell)
+       ("gtkspell" ,gtkspell3)
        ("libnsl" ,libnsl)
        ("openldap" ,openldap)
        ("openssl" ,openssl)))
