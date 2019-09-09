0d7681a2b5169c174ea98a102727a68277ef749b
Eric Brown <brown@fastmail.com>
2019-09-08 20:08:54 -0500

    ufraw.scm: Fix URI

--- ufraw.scm
+++ ufraw.scm
@@ -34,7 +34,7 @@
     (source (origin
              (method url-fetch)
              (uri (list
-                   (string-append "httpsC://sourceforge.net/projects/ufraw/files/ufraw/"
+                   (string-append "https://sourceforge.net/projects/ufraw/files/ufraw/"
                                   name "-" version "/ufraw-" version ".tar.gz")))
              (sha256
               (base32
