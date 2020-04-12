a7f273aae347930a3ae33532d2a92b3b8abcd786
Eric Brown <ecbrown@ericcbrown.com>
2020-04-12 16:40:34 -0500

    r-rserve fixes

--- r-rserve.scm
+++ r-rserve.scm
@@ -21,6 +21,8 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix build-system gnu)
+  #:use-module (guix build-system r)
+  #:use-module (guix build-system trivial)
   #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module (gnu packages)
